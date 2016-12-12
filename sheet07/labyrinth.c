1a90f0b8de82e427295295d650430c94a61ae8b2
Squifi <php@mcdougall.de>
2016-12-12 18:24:30 +0100

    Improved Labyrinth Generator

--- sheet07/labyrinth.c
+++ sheet07/labyrinth.c
@@ -1,26 +1,32 @@
+#include <time.h>
 #include <stdio.h>
 #define N 10
 #define M 10
-#define WALL '*'
+#define WALL '@'
 #define MAUS 'o'
 
 char labyrinth[N][M]; 
+int isExitDrawn = 0;
 
-void ResetLabyrinth(int x, int y) {
+int GetRandomValue() {
+   srand(time(NULL));
+   return rand()%M;
+}
+
+void ResetLabyrinth(int x, int y, int randomValue) {
    if(x == 0 || y==0 || x==M || y==N) {
-      labyrinth[x][y] = WALL; 
+      if(isExitDrawn<2 && randomValue==y) {
+         isExitDrawn++;
+         labyrinth[x][y] = ' ';
+      } else if(x==N-1 && y==M && isExitDrawn==0) {
+         labyrinth[x][y] = ' ';
+      } else {
+         labyrinth[x][y] = WALL; 
+      }
    } else if(N/2==x && M/2==y) {
       labyrinth[x][y] = MAUS;
    } else {
       labyrinth[x][y] = ' ';
-   }
-}
-
-void Init() {
-   for(int y = 0; y <= M; ++y) {
-      for(int x=0; x<=N; ++x) {
-        ResetLabyrinth(x,y); 
-      }
    }
 }
 
@@ -33,8 +39,17 @@ void PrintLabyrinth() {
    }
 }
 
+void Init() {
+   int randomValue = GetRandomValue();
+   for(int y = 0; y <= M; ++y) {
+      for(int x=0; x<=N; ++x) {
+        ResetLabyrinth(x,y,randomValue); 
+      }
+   }
+   PrintLabyrinth();
+}
+
 int main() {
    Init();
-   PrintLabyrinth();
    return 0;
 }
