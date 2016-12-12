568d18998c3ac89afde15f8cecba8c10fc9dbe91
Squifi <php@mcdougall.de>
2016-12-12 18:53:49 +0100

    Implements first assignment without walls in labyrinth

--- sheet07/labyrinth.c
+++ sheet07/labyrinth.c
@@ -4,36 +4,37 @@
 #define M 10
 #define WALL '@'
 #define MAUS 'o'
+#define START_MAUS_X N/2
+#define START_MAUS_Y M/2
 
-char labyrinth[N][M]; 
+char pos[N][M]; 
 int isExitDrawn = 0;
 
 int GetRandomValue() {
    srand(time(NULL));
-   return rand()%M;
+   return rand()%M-2+1;
 }
 
 void ResetLabyrinth(int x, int y, int randomValue) {
    if(x == 0 || y==0 || x==M || y==N) {
       if(isExitDrawn<2 && randomValue==y) {
          isExitDrawn++;
-         labyrinth[x][y] = ' ';
-      } else if(x==N-1 && y==M && isExitDrawn==0) {
-         labyrinth[x][y] = ' ';
+         pos[x][y] = ' ';
+      } else if(x==N-1 && y==M && isExitDrawn<2) {
+         pos[x][y] = ' ';
       } else {
-         labyrinth[x][y] = WALL; 
+         pos[x][y] = WALL; 
       }
-   } else if(N/2==x && M/2==y) {
-      labyrinth[x][y] = MAUS;
    } else {
-      labyrinth[x][y] = ' ';
+
+      pos[x][y] = ' ';
    }
 }
 
 void PrintLabyrinth() {
    for(int y = 0; y <= M; ++y) {
       for(int x=0; x<=N; ++x) {
-         printf("%c", labyrinth[x][y]);
+         printf("%c", pos[x][y]);
       }
       printf("\n");
    }
@@ -46,10 +47,33 @@ void Init() {
         ResetLabyrinth(x,y,randomValue); 
       }
    }
-   PrintLabyrinth();
+}
+
+int maus(int x, int y) {
+   pos[x][y] = MAUS;
+
+   int north = pos[x][y-1];
+   int east = pos[x+1][y];
+   int south = pos[x][y+1];
+   int west = pos[x-1][y];
+
+   if(x==M || y==M && pos[x][y]==' ') {
+      return 1;
+   } else if(north==' '){
+      maus(x, y-1);     
+   } else if(east==' ') {
+      maus(x+1, y);     
+   } else if(south==' ') {
+      maus(x, y+1);     
+   } else if(west==' ') {
+      maus(x-1,y);     
+   }
+   return 0;
 }
 
 int main() {
    Init();
+   maus(START_MAUS_X,START_MAUS_Y);
+   PrintLabyrinth();
    return 0;
 }
